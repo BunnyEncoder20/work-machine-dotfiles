c702c475e6b33b6f42bbae956db2b4e13ded69e9
Varun Verma <varun.verma2024@gmail.com>
2026-04-10 14:02:43 +0530

    fix of sql formatting.

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -3,8 +3,9 @@ return {
   opts = {
     formatters = {
       sqlfluff = {
-        -- Force sqlfluff to use the sqlite dialect
-        args = { "format", "--dialect=sqlite", "-" },
+        args = { "fix", "--disable-progress-bar", "--nocolor", "--quiet", "-" },
+        -- Tell conform that exit code 1 is a valid success state!
+        exit_codes = { 0, 1 },
       },
     },
     formatters_by_ft = {
